d18f7db65a295dba9264cdbf15dd389c13d1fb9a
Michael Pederson <rlwimi@gmail.com>
2017-05-26 15:57:04 -0500

    Integrate touch visualizer for demoing.

--- MajorInput/AppDelegate.swift
+++ MajorInput/AppDelegate.swift
@@ -1,4 +1,5 @@
 import UIKit
+import TouchVisualizer
 
 @UIApplicationMain
 final class AppDelegate: UIResponder, UIApplicationDelegate  {
@@ -10,6 +11,9 @@ final class AppDelegate: UIResponder, UIApplicationDelegate  {
   }()
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    var config = Configuration()
+    config.color = .red
+    Visualizer.start(config)
     let window = UIWindow(frame: UIScreen.main.bounds)
     window.rootViewController = builder.makeAppNavigationController()
     window.makeKeyAndVisible()
